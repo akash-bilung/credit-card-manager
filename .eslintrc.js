e66e3332cfa14f99fd7696839b14dd1135c41d87
akash-prescriber-360 <47534408+akash-prescriber-360@users.noreply.github.com>
2022-03-03 14:56:29 +0530

    Modify: folder structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,11 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
+    quotes: ["error", "double"],
+    // we use 2 spaces to indent our code
+    indent: ["error", 2],
+    // we want to avoid extraneous spaces
+    "no-multi-spaces": ["error"],
   },
 };
