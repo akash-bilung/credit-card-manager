29696c9401c77314efb410b084ee2931e72af19f
akash-prescriber-360 <47534408+akash-prescriber-360@users.noreply.github.com>
2022-03-05 02:21:16 +0530

    Add: Debit card

---
+++ src/components/Card/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="card">
+    <div class="card__logo">
+      <img :src="card.bank" alt="" />
+    </div>
+    <div class="card__title font--bold">{{ card.title }}</div>
+    <ul class="card__number">
+      <li v-for="(number, index) in card.numbers" :key="index">
+        <span class="font--md" v-if="isFirstThree(index)">
+          &#9679; &#9679; &#9679; &#9679;
+        </span>
+        <span v-else>{{ number }}</span>
+      </li>
+    </ul>
+    <ul class="card__details">
+      <li>
+        <span class="font--bold">Thru:</span>
+        <span>{{ moment(card.expiry).format("MM/YY") }}</span>
+      </li>
+      <li>
+        <span class="font--bold">CVV:</span>
+        <span class="font--lg">{{ card.cvv }}</span>
+      </li>
+    </ul>
+    <div class="card__type">
+      <img :src="card.type" alt="" />
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  props: {
+    card: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  created() {
+    this.moment = moment;
+  },
+  methods: {
+    isFirstThree(index) {
+      return !this.card.visible && index + 1 !== this.card.numbers.length;
+    },
+  },
+  computed: {},
+};
+</script>
+
+<style lang="scss">
+.card {
+  background: $primary;
+  border-radius: 1rem;
+  padding: 2.8rem;
+  color: $white;
+  &__logo {
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 2.8rem;
+    & > img {
+      width: 100%;
+      max-width: 8.3rem;
+    }
+  }
+  &__title {
+    font-size: 2.4rem;
+    margin-bottom: 3.3rem;
+  }
+  &__number {
+    margin-bottom: 1.7rem;
+    display: flex;
+    align-items: center;
+    & > li {
+      &:not(:last-child) {
+        margin-right: 2.7rem;
+      }
+      font-size: 1.4rem;
+    }
+  }
+  &__details {
+    display: flex;
+    margin-bottom: 0.5rem;
+    & > li {
+      &:not(:last-child) {
+        margin-right: 3.7rem;
+      }
+      font-size: 1.3rem;
+      display: flex;
+      & > span {
+        &:not(:last-child) {
+          margin-right: 0.5rem;
+        }
+        &.font--lg {
+          line-height: 1em;
+        }
+      }
+    }
+  }
+  &__type {
+    display: flex;
+    justify-content: flex-end;
+    & > img {
+      width: 100%;
+      max-width: 6.7rem;
+    }
+  }
+}
+</style>
--- src/pages/cards/index.vue
+++ src/pages/cards/index.vue
@@ -2,8 +2,12 @@
   <content-box>
     <content-header />
     <tabs>
-      <tab-item title="My debit cards">Data 1</tab-item>
-      <tab-item title="All company cards">Data 2</tab-item>
+      <tab-item title="My debit cards">
+        <card :card="card" />
+      </tab-item>
+      <tab-item title="All company cards">
+        <h1>No Content</h1>
+      </tab-item>
     </tabs>
   </content-box>
 </template>
@@ -13,12 +17,34 @@ import ContentHeader from "@/components/Content/Header";
 
 import Tabs from "@/components/Ui/Tab";
 import TabItem from "@/components/Ui/Tab/Item";
+
+import Card from "@/components/Card";
 export default {
+  data() {
+    return {
+      isNumberVisible: false,
+    };
+  },
   components: {
     ContentBox,
     ContentHeader,
     Tabs,
     TabItem,
+    Card,
+  },
+  computed: {
+    card() {
+      let card = {
+        visible: this.isNumberVisible,
+        bank: "/images/base/Logo-light.svg",
+        title: "Mark Henry",
+        numbers: ["1234", "5678", "9012", "3456"],
+        expiry: new Date(),
+        cvv: "***",
+        type: "/icons/Visa.svg",
+      };
+      return card;
+    },
   },
 };
 </script>
